ce5e6e925f9bb6d943a74121446db5d55dc42d17
teooko <teobogdann@yahoo.com>
2024-02-13 18:50:22 +0200

    Add presentation project

---
+++ TaskProjectBackend.Presentation/Controllers/TaskController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskProjectBackend.Presentation.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class TaskController : Controller
+{
+
+    /*
+    private readonly GenreService _genreService = new GenreService();
+    
+    [HttpPost]
+    public ActionResult<Task> Post([FromBody] DGenre genre)
+    {
+        DGenre postedGenre = _genreService.Post(genre);
+        
+        if(postedGenre == null)
+            return this.BadRequest("Name required");
+            
+        return this.Ok(postedGenre);
+    }
+    
+    [HttpGet]
+    public ActionResult<DGenre> Get()
+    {
+        return this.Ok(_genreService.Get());
+    }
+    
+    */
+}
