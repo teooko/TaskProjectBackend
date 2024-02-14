6373f6179fbb457094e77d7fa835a3dc9a3a2466
teooko <teobogdann@yahoo.com>
2024-02-14 18:47:40 +0200

    Add TaskService

---
+++ TaskProjectBackend.Application/Services/TaskService.cs
@@ -0,0 +1,24 @@
+﻿using TaskProjectBackend.DataAccess.Repositories;
+
+namespace TaskProjectBackend.Application.Services;
+
+public class TaskService
+{
+    private readonly TaskRepository _taskRepository;
+
+    public TaskService()
+    {
+        _taskRepository = new TaskRepository();
+    }
+
+    public Domain.Task Get(int id)
+    {
+        var task = _taskRepository.Get(id);
+        return task;
+    }
+
+    public Domain.Task Post(Domain.Task task)
+    {
+        return _taskRepository.Post(task);
+    }
+}
